66ac92633209d52e7c29ea4ae968258d0d0756cb
ByeongHoonPark <eep7942@gmail.com>
2016-01-31 20:06:18 +0900

    IniFile_Class

---
+++ Server_Library/Server_Library/IniFile.h
@@ -0,0 +1,23 @@
+#pragma once
+class CIniFile
+{
+	TCHAR	m_FileName[MAX_PATH];
+
+public:
+
+	BOOL	Open(LPCTSTR fileName);
+	BOOL	Close(VOID);
+	BOOL	SetValue(LPCTSTR keyName, LPCTSTR valueName, LPCTSTR value);
+	BOOL	SetValue(LPCTSTR keyName, LPCTSTR valueName, DWORD value);
+	BOOL	SetValue(LPCTSTR keyName, LPCTSTR valueName, FLOAT value);
+
+	BOOL	GetValue(LPCTSTR keyName, LPCTSTR valueName, LPTSTR value, LPDWORD bufferLength);
+	BOOL	GetValue(LPCTSTR keyName, LPCTSTR valueName, LPDWORD value);
+	BOOL	GetValue(LPCTSTR keyName, LPCTSTR valueName, FLOAT* value);
+
+public:
+	CIniFile();
+	~CIniFile();
+};
+
+
